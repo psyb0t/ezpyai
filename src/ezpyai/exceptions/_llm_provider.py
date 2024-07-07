58cd374bf4fc7d95bd2bc448ff047378fded3edc
Ciprian Mandache <psyb0t@51k.eu>
2024-07-07 04:58:26 +0300

    start refactoring exc

--- src/ezpyai/exceptions/_llm_provider.py
+++ src/ezpyai/exceptions/_llm_provider.py
@@ -1,13 +1,15 @@
-# LLMProvider Exceptions
+from typing import Any
+
+
 class UnsupportedModelError(Exception):
     """Exception raised when an unsupported model is used."""
 
-    def __init__(self, message="Unsupported model", *args):
+    def __init__(self, message: str = "Unsupported model", *args: Any) -> None:
         super().__init__(message, *args)
 
 
 class UnsupportedLoraError(Exception):
     """Exception raised when an unsupported lora is used."""
 
-    def __init__(self, message="Unsupported lora", *args):
+    def __init__(self, message: str = "Unsupported lora", *args: Any):
         super().__init__(message, *args)
